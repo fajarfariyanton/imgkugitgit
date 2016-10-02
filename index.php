103c3443d34df38bd8347dc446881cf23fad8f4b
fajarfariyanton <fajar.fariyanton@gmail.com>
2016-10-02 16:31:30 +0700

    Update index.php

--- index.php
+++ index.php
@@ -40,11 +40,21 @@ preg_match('/https?:\/\/([a-zA-Z0-9-_.]+)\/(.*)/i', $URL, $outdomain);
      $hasil = curl_exec($data);
      curl_close($data);
 
-	 
+if(strlen($hasil) < 150){
+	return 'error';
+}	
 	$fff= fopen("cache_gambar/".$filesname,"w");
 	fwrite($fff, $hasil);
 	fclose($fff);
 return $hasil;	 
+}
+
+
+//CODING happy
+if(!file_exists("cache_gambar")){
+		$oldmask = umask(0);
+		mkdir("cache_gambar", 0777);
+		umask($oldmask);
 }
 
 	if(preg_match('/^\/https?:\/\//i', $_SERVER['REQUEST_URI'])){
@@ -52,4 +62,27 @@ $image_url= preg_replace('/^\/(https?:\/\/)/i', '\\1', $_SERVER['REQUEST_URI']);
 	}else{
 $image_url= preg_replace('/^\//i', 'http://', $_SERVER['REQUEST_URI']);
 	}
-echo $image_url;
+
+$image_string= grab_image($image_url);
+
+	if($image_string == 'error'){
+header('HTTP/1.1 400 Bad Request');
+header("Content-type: text/plain");
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+header("Cache-Control: no-cache");
+header("Pragma: no-cache");
+		exit('Error 0004. Unable to load the image.');
+	}
+
+
+
+	if (isset($_SERVER['HTTP_IF_MODIFIED_SINCE'])){
+		header('HTTP/1.1 304 Not Modified');
+        die();
+	}
+header('Content-Type: image/jpeg');
+header('Cache-control: max-age='.(60*60*365*2));
+header('Expires: '.gmdate(DATE_RFC1123,time()+60*60*365*2));
+header('Last-Modified: '.gmdate(DATE_RFC1123,time()));
+
+echo $image_string;
